dc355efb4d58ad251ce0b50d20b7813c8ac0ecde
Amy Cen <amy.cen@blackrock.com>
2019-06-02 02:42:58 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
+class Course
+  attr_accessors :title, :schedule, :description
+  @@all = []
 
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+end
+d
 
